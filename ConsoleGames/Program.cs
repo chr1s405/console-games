b46c856e55aca8859b0394b40cfe2f2e2d488189
chr1s405 <christian.arthurmens@gmail.com>
2025-06-19 04:55:03 +0200

    program.cs up to date with snakeGame

--- ConsoleGames/Program.cs
+++ ConsoleGames/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
 using System.IO;
 using System.Reflection;
-using static ConsoleGames.Program;
 using Microsoft.Win32.SafeHandles;
 using System.Xml.Serialization;
+using Utils;
 
 namespace ConsoleGames
 {
@@ -14,35 +14,50 @@ namespace ConsoleGames
             int choice;
             do
             {
+                choice = 0;
                 Console.WriteLine("MENU");
                 Console.WriteLine("1. GravityGame");
                 Console.WriteLine("2. Snake");
                 Console.WriteLine("3. PathFinding");
-                //Console.WriteLine("4. PathMap");
                 Console.WriteLine("0. quit");
                 Console.Write("Select option: ");
                 try
                 {
                     choice = Convert.ToInt32(Console.ReadLine());
                 }
-                catch
-                {
-                    choice = 0;
-                }
-                Console.Clear();
-                switch (choice)
-                {
-                    case 1: GravityGame.Play(); break;
-                    case 2: Snake.Play(); break;
-                    case 3: Pathfinding.Play(); break;
-                }
+                catch { }
                 if (choice != 0)
                 {
+                    Console.Clear();
+                    switch (choice)
+                    {
+                        case 1: GravityGame.Play(); break;
+                        case 2: SnakeGame.Play(); break;
+                        case 3: Pathfinding.Play(); break;
+                        case -1: TryingThingsOut(); break;
+                    }
                     Console.WriteLine($"Press enter to continue");
                     Console.ReadLine();
                     Console.Clear();
                 }
+                else
+                {
+                    Console.WriteLine("Thanks for playing");
+                }
             } while (choice != 0);
+        }
+        static public void TryingThingsOut()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("EXPERIMENTING & en testig");
+            Console.WriteLine("=========================");
+            Console.WriteLine(new Point2I(2, 6));
+            Console.WriteLine(new Point2I(2, 6) + (4, 2));
+            Console.WriteLine(new Point2I(2, 6) - (4, 2));
+            Console.WriteLine(new Point2I(2, 6) - Dir.left);
+            Console.WriteLine(new Point2I(2, 6) - Dir.right);
+            Console.WriteLine(new Point2I(2, 6) - Dir.up);
+            Console.WriteLine(new Point2I(2, 6) + Dir.up);
         }
     }
 }
